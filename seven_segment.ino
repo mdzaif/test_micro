53604b219ce0f082de9176e9ffbf8e0eedb30bdb
mdzaif <mdizaif@gmail.com>
2024-11-06 17:49:43 +0600

    [FIX]: Condition error solve

--- seven_segment.ino
+++ seven_segment.ino
@@ -13,102 +13,102 @@ void loop()
 {
   switch(num){
 	case 0:
-	digitalWrite(2, LOW);
-	digitalWrite(3, LOW);
-	digitalWrite(4, LOW);
-	digitalWrite(5, LOW);
-	digitalWrite(6, LOW);
-	digitalWrite(7, LOW);
-	digitalWrite(8, HIGH);
-	break;
-  case 1:
 	digitalWrite(2, HIGH);
-	digitalWrite(3, LOW);
-	digitalWrite(4, LOW);
+	digitalWrite(3, HIGH);
+	digitalWrite(4, HIGH);
 	digitalWrite(5, HIGH);
 	digitalWrite(6, HIGH);
 	digitalWrite(7, HIGH);
-	digitalWrite(8, HIGH);
+	digitalWrite(8, LOW);
+	break;
+  case 1:
+	digitalWrite(2, LOW);
+	digitalWrite(3, HIGH);
+	digitalWrite(4, HIGH);
+	digitalWrite(5, LOW);
+	digitalWrite(6, LOW);
+	digitalWrite(7, LOW);
+	digitalWrite(8, LOW);
 	break;
   
   case 2:
-	digitalWrite(2, LOW);
-	digitalWrite(4, HIGH);
-	digitalWrite(3, LOW);
-	digitalWrite(5, LOW);
-	digitalWrite(6, LOW);
-	digitalWrite(7, HIGH);
-	digitalWrite(8, LOW);
-	break;
-  
-  case 3:
-	digitalWrite(2, LOW);
-	digitalWrite(3, LOW);
-	digitalWrite(4, LOW);
-	digitalWrite(5, LOW);
-	digitalWrite(6, HIGH);
-	digitalWrite(7, HIGH);
-	digitalWrite(8, LOW);
-	break;
-  
-  case 4:
 	digitalWrite(2, HIGH);
-	digitalWrite(3, LOW);
 	digitalWrite(4, LOW);
+	digitalWrite(3, HIGH);
 	digitalWrite(5, HIGH);
 	digitalWrite(6, HIGH);
 	digitalWrite(7, LOW);
-	digitalWrite(8, LOW);
+	digitalWrite(8, HIGH);
+	break;
+  
+  case 3:
+	digitalWrite(2, HIGH);
+	digitalWrite(3, HIGH);
+	digitalWrite(4, HIGH);
+	digitalWrite(5, HIGH);
+	digitalWrite(6, LOW);
+	digitalWrite(7, LOW);
+	digitalWrite(8, HIGH);
+	break;
+  
+  case 4:
+	digitalWrite(2, LOW);
+	digitalWrite(3, HIGH);
+	digitalWrite(4, HIGH);
+	digitalWrite(5, LOW);
+	digitalWrite(6, LOW);
+	digitalWrite(7, HIGH);
+	digitalWrite(8, HIGH);
 	break;
   
   case 5:
-	digitalWrite(2, LOW);
-	digitalWrite(3, HIGH);
-	digitalWrite(4, LOW);
-	digitalWrite(5, LOW);
-	digitalWrite(6, HIGH);
-	digitalWrite(7, LOW);
+	digitalWrite(2, HIGH);
+	digitalWrite(3, LOW);
+	digitalWrite(4, HIGH);
+	digitalWrite(5, HIGH);
+	digitalWrite(6, LOW);
+	digitalWrite(7, HIGH);
 	digitalWrite(8, LOW);
 	break;
   
   case 6:
-	digitalWrite(2, LOW);
-	digitalWrite(3, HIGH);
-	digitalWrite(4, LOW);
-	digitalWrite(5, LOW);
-	digitalWrite(6, LOW);
-	digitalWrite(7, LOW);
-	digitalWrite(8, LOW); 
+	digitalWrite(2, HIGH);
+	digitalWrite(3, LOW);
+	digitalWrite(4, HIGH);
+	digitalWrite(5, HIGH);
+	digitalWrite(6, HIGH);
+	digitalWrite(7, HIGH);
+	digitalWrite(8, HIGH); 
 	break;
   
   case 7:
-	digitalWrite(2, LOW);
-	digitalWrite(3, LOW);
-	digitalWrite(4, LOW);
+	digitalWrite(2, HIGH);
+	digitalWrite(3, HIGH);
+	digitalWrite(4, HIGH);
+	digitalWrite(5, LOW);
+	digitalWrite(6, LOW);
+	digitalWrite(7, LOW);
+	digitalWrite(8, LOW);
+	break;
+  
+  case 8:
+	digitalWrite(2, HIGH);
+	digitalWrite(3, HIGH);
+	digitalWrite(4, HIGH);
 	digitalWrite(5, HIGH);
 	digitalWrite(6, HIGH);
 	digitalWrite(7, HIGH);
 	digitalWrite(8, HIGH);
 	break;
   
-  case 8:
-	digitalWrite(2, LOW);
-	digitalWrite(3, LOW);
-	digitalWrite(4, LOW);
-	digitalWrite(5, LOW);
-	digitalWrite(6, LOW);
-	digitalWrite(7, LOW);
-	digitalWrite(8, LOW);
-	break;
-  
   case 9:
-	digitalWrite(2, LOW);
-	digitalWrite(3, LOW);
-	digitalWrite(4, LOW);
-	digitalWrite(6, HIGH);
-	digitalWrite(5, LOW);
-	digitalWrite(7, LOW);
-	digitalWrite(8, LOW);
+	digitalWrite(2, HIGH);
+	digitalWrite(3, HIGH);
+	digitalWrite(4, HIGH);
+	digitalWrite(6, LOW);
+	digitalWrite(5, HIGH);
+	digitalWrite(7, HIGH);
+	digitalWrite(8, HIGH);
 	break;
   }
   num++;
